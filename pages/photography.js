4f6b6c83c62cd42f0f49e04d21859d8f72c58977
Dante Tobar <dante.m.tobar@gmail.com>
2020-07-26 09:27:07 -0700

    more photos added;

--- pages/photography.js
+++ pages/photography.js
@@ -8,9 +8,27 @@ export default function Photography() {
   }, []);
   return (
     <div id="photography" className={`${mounting ? 'hide' : 'show'}`}>
-      <img src="https://tobar-portfolio.s3-us-west-1.amazonaws.com/antonforegg.jpg" />
-      <img src="https://tobar-portfolio.s3-us-west-1.amazonaws.com/sandm.jpg" />
-      <img src="https://tobar-portfolio.s3-us-west-1.amazonaws.com/britforegg.jpg" />
+      <img src="/squares/rob2.jpg" />
+      <img src="/squares/seanpromo.jpg" />
+      <img src="/squares/rcpromo.jpg" />
+      <img src="/squares/greg.jpg" />
+      <img src="/squares/haze1.jpg" />
+      <img src="/squares/ez.jpg" />
+      <img src="/squares/antonforegg.jpg" />
+      <img src="/squares/britforegg.jpg" />
+      <img src="/squares/jarrenfarefax.jpg" />
+      <img src="/squares/mario.jpg" />
+      <img src="/squares/cow.jpg" />
+      <img src="/squares/jarenkit.jpg" />
+      <img src="/squares/sauciego.jpg" />
+      <img src="/squares/seanforegg.jpg" />
+      <img src="/squares/sandm.jpg" />
+      <img src="/squares/hightide.jpg" />
+      <img src="/squares/educatedboys.jpg" />
+      <img src="/squares/lunasboots.jpg" />
+      <img src="/squares/burntcar.jpg" />
+      <img src="/squares/oxnard6.jpg" />
+      <img src="/squares/vanillasky.jpg" />
     </div>
   );
 }

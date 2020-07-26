7423cd1cc32d49742d94bb40125ac04a4f8aba7f
Dante Tobar <dante.m.tobar@gmail.com>
2020-07-26 09:25:27 -0700

    header split into mobile and desktop headers

---
+++ pages/desktopHeader.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+
+export default function DesktopHeader(props) {
+  const [currentUnderline, setUnderline] = useState(props.selectedPage);
+  const [direction, setDirection] = useState(null);
+  const [mounting, setMounting] = useState(true);
+
+  useEffect(() => {
+    setDirection([currentUnderline, props.selectedPage]);
+    setTimeout(() => setUnderline(props.selectedPage), 50);
+  }, [props.selectedPage]);
+
+  useEffect(() => {
+    setTimeout(() => setMounting(false), 30);
+  }, []);
+
+  const getClass = id => {
+    let className = currentUnderline === id ? 'underline' : '';
+    if (!direction) return className;
+
+    if (id === direction[0]) {
+      className += id < direction[1] ? ' underline-right' : ' underline-left';
+    } else if (id === direction[1]) {
+      className += id < direction[0] ? ' underline-right' : ' underline-left';
+    }
+
+    return className;
+  };
+
+  return (
+    <div
+      id="desktop-header"
+      className={`header ${mounting || props.hide ? 'hide' : 'show'} ${
+        props.selectedPage ? '' : 'expand'
+      }`}
+    >
+      <div className="tabs">
+        <h2 className={getClass(1)} onClick={() => props.selectPage(1)}>
+          projects
+          <span />
+        </h2>
+        <h2 className={getClass(2)} onClick={() => props.selectPage(2)}>
+          photography
+          <span />
+        </h2>
+      </div>
+      <div className="name">
+        <h1>dante</h1>
+        <h1>tobar</h1>
+      </div>
+      <div className="tabs">
+        <h2 className={getClass(3)} onClick={() => props.selectPage(3)}>
+          about
+          <span />
+        </h2>
+        <h2 className={getClass(4)} onClick={() => props.selectPage(4)}>
+          contact
+          <span />
+        </h2>
+      </div>
+    </div>
+  );
+}
---
+++ pages/mobileHeader.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+
+export default function MobileHeader(props) {
+  const [showMenu, setShowMenu] = useState(false);
+  const [unmounting, setUnmounting] = useState(false);
+
+  const openPage = page => {
+    setUnmounting(true);
+    props.selectPage(null);
+    setTimeout(() => {
+      props.selectPage(page);
+      setUnmounting(false);
+      setShowMenu(false);
+    }, 200);
+  };
+
+  return (
+    <React.Fragment>
+      <div
+        id="mobile-header"
+        className={`${props.selectedPage ? '' : 'expand'} ${
+          showMenu ? 'hide' : 'show'
+        } header`}
+      >
+        <h2>dante tobar</h2>
+        <button
+          type="button"
+          className="icon-button"
+          onClick={() => setShowMenu(true)}
+        >
+          <MenuIcon style={{ fontSize: 40 }} />
+        </button>
+      </div>
+      <div
+        id="menu"
+        className={`${unmounting ? 'unmounting' : showMenu ? 'show' : 'hide'}`}
+      >
+        <button
+          type="button"
+          className="icon-button close"
+          onClick={() => setShowMenu(false)}
+        >
+          <CloseIcon style={{ fontSize: 40 }} />
+        </button>
+        <h1
+          onClick={() => {
+            openPage(1);
+          }}
+        >
+          projects
+          <span />
+        </h1>
+        <h1
+          onClick={() => {
+            openPage(2);
+          }}
+        >
+          photography
+          <span />
+        </h1>
+        <h1
+          onClick={() => {
+            openPage(3);
+          }}
+        >
+          about
+          <span />
+        </h1>
+        <h1
+          onClick={() => {
+            openPage(4);
+          }}
+        >
+          contact
+          <span />
+        </h1>
+      </div>
+    </React.Fragment>
+  );
+}
